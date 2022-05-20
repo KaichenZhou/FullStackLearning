d871b2de92a5065c8d5e4fe3632655c380872b79
Kaichen Zhou <71943412+KaichenZhou@users.noreply.github.com>
2022-05-20 15:25:23 -0400

    Add files via upload
    HW Day3 SQL

---
+++ Day3SQL.sql
@@ -0,0 +1,30 @@
+
+/*
+create view view_product_order_Zhou as
+select p.ProductName, sum(od.Quantity) 'total_quantity'
+from Products p
+inner join [Order Details] od
+on p.ProductID = od.ProductID
+group by p.ProductName
+--order by total_quantity desc
+*/
+
+
+create proc sp_product_order_quantity_zhou
+@product_id int,
+@total_quantity varchar(20) out
+as
+begin
+select @product_id= p.productid,
+@total_quantity = sum(od.Quantity)
+from Products p
+inner join [Order Details] od
+on p.ProductID = od.ProductID 
+group by p.ProductID
+end
+
+exec sp_product_order_quantity_zhou
+
+
+
+
