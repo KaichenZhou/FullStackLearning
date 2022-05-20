a167d4acf162eb92873ee094ff7a84967d1e7c07
Kaichen Zhou <71943412+KaichenZhou@users.noreply.github.com>
2022-05-20 15:27:00 -0400

    Update Day3SQL.sql

--- Day3SQL.sql
+++ Day3SQL.sql
@@ -1,15 +1,16 @@
-
-/*
-create view view_product_order_Zhou as
+--Day3 SQL
+--1. Create a view named “view_product_order_[your_last_name]”, 
+--list all products and total ordered quantity for that product.
+create view view_product_order_Zhou 
+as
 select p.ProductName, sum(od.Quantity) 'total_quantity'
 from Products p
 inner join [Order Details] od
 on p.ProductID = od.ProductID
 group by p.ProductName
---order by total_quantity desc
-*/
 
-
+--2.Create a stored procedure “sp_product_order_quantity_[your_last_name]” 
+--that accept product id as an input and total quantities of order as output parameter.
 create proc sp_product_order_quantity_zhou
 @product_id int,
 @total_quantity varchar(20) out
@@ -23,8 +24,11 @@ on p.ProductID = od.ProductID
 group by p.ProductID
 end
 
-exec sp_product_order_quantity_zhou
+--Create a stored procedure “sp_product_order_city_[your_last_name]” 
+--that accept product name as an input and top 5 cities that ordered most 
+--that product combined with the total quantity of that product ordered from that city as output.
 
-
+
+
 
 
