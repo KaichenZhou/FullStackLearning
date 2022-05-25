9ac022dfd86ab35708bd5b81fab33a03ebf7b424
Kaichen Zhou <71943412+KaichenZhou@users.noreply.github.com>
2022-05-25 16:36:55 -0400

    Update day5csharp.cs

--- day5csharp.cs
+++ day5csharp.cs
@@ -1,5 +1,5 @@
-﻿/*
- * Day5 hw
+/*
+ * Day5 hw Assignment A
  * Test your Knowledge
  * 1. What type would you choose for the following “numbers”?
  * A person’s telephone number = int
@@ -366,4 +366,449 @@ namespace day5Csharp
         }
     }    
 }
+
+ * Day5 hw Assignment B
+ * Test your Knowledge
+1. When to use String vs. StringBuilder in C# ?
+Using StringBuilder When I need a mutable string.
+
+2. What is the base class for all arrays in C#?
+The array class
+
+3. How do you sort an array in C#?
+Array.Sort();
+
+4. What property of an array object can be used to get the total number of elements in an array?
+Length
+
+5. Can you store multiple data types in System.Array?
+No
+
+6. What’s the difference between the System.Array.CopyTo() and System.Array.Clone()?
+The Clone() method returns a new array (a shallow copy) object containing all the elements in the original array. 
+The CopyTo() method copies the elements into another existing array. 
+
+Practice Arrays
+1. Copying an Array
+Write code to create a copy of an array. First, start by creating an initial array. (You can use
+whatever type of data you want.) Let’s start with 10 items. Declare an array variable and
+assign it a new array with 10 items in it. Use the things we’ve discussed to put some values
+in the array.
+Now create a second array variable. Give it a new array with the same length as the first.
+Instead of using a number for this length, use the Lengthproperty to get the size of the
+original array.
+Use a loop to read values from the original array and place them in the new array. Also
+print out the contents of both arrays, to be sure everything copied correctly
+
+using System;
+namespace day5csharp
+{
+    class day5csharp
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Total length of array is " + num.Length);
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine("The elements in original array are " + num[i]);
+            }
+            var num2 = new int[num.Length];
+            for (int i = 0; i < num.Length; i++)
+            {
+                num2[i] = num[i];
+                Console.WriteLine("The elements in new array are " + num2[i]);
+            }
+        }
+    }
+}
+
+2.Write a simple program that lets the user manage a list of elements. It can be a grocery list,
+"to do" list, etc. Refer to Looping Based on a Logical Expression if necessary to see how to
+implement an infinite loop. Each time through the loop, ask the user to perform an
+operation, and then show the current contents of their list. The operations available should
+be Add, Remove, and Clear. The syntax should be as follows:
++ some item
+- some item
+--
+Your program should read in the user's input and determine if it begins with a “+” or “-“ or
+if it is simply “—“ . In the first two cases, your program should add or remove the string
+given ("some item" in the example). If the user enters just “—“ then the program should
+clear the current list. Your program can start each iteration through its loop with the
+following instruction:
+Console.WriteLine("Enter command (+ item, - item, or -- to clear)):");
+*/
+using System;
+namespace day5csharp
+{
+    class day5csharp
+    {
+        static void add()
+        {
+            string[] store = new string[10];
+            List<string> arr = store.ToList();
+
+            //string[] arr = new string[] { "Hi" };
+            //List<string> ls = arr.ToList();
+            arr.Add("Apple");
+            arr.Add("Orange");
+            arr.Add("Milk");
+
+            store = arr.ToArray();
+            foreach (var e in store)
+            {
+                Console.WriteLine(e);
+            }
+        }
+       
+        static void Main(string[] args)
+        {
+            add();
+        }
+    }
+}
+
+
+
+/*
+
+3. Write a method that calculates all prime numbers in given range and returns them as array
+of integers
+
+using System;
+namespace day5csharp
+{
+    class day5csharp
+    {
+        static int[] FindPrimesInRange(int startNum, int endNum)  //startNum, endNum
+        {
+            int[] p = new int[endNum - startNum + 1];
+            int i, j;
+            int arrayIndex = 0;
+            for (i = startNum; i <= endNum; i++)
+            {
+                if (i == 1 || i == 0)
+                {
+                    continue;
+                }
+                int flag = 1;
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag == 1)
+                {
+                    p[arrayIndex++] = i;
+                }
+            }
+            int[] arr = new int[arrayIndex];
+            for (i = 0; i < arrayIndex; i++)
+            {
+                arr[i] = p[i];
+            }
+
+            return arr;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter starting range and ending range of prime numbers");
+            int startNum = Convert.ToInt32(Console.ReadLine());
+            int endNum = Convert.ToInt32(Console.ReadLine());
+            int[] primes = FindPrimesInRange(startNum, endNum);
+            Console.WriteLine(string.Join(" ", primes));
+        }
+    }
+}
+
+
+4. Write a program to read an array of n integers (space separated on a single line) and an
+integer k, rotate the array right k times and sum the obtained arrays after each rotation as
+shown below.
+After r rotations the element at position I goes to position (I + r) % n.
+The sum[] array can be calculated by two nested loops: for r = 1 ... k; for I = 0 ... n-1.
+Input Output Comments
+3 2 4 -1 3 2 5 6 rotated1[] = -1 3 2 4
+2 rotated2[] = 4 -1 3 2
+sum[] = 3 2 5 6
+1 2 3 4 5 12 10 8 6 9 rotated1[] = 5 1 2 3 4
+3 rotated2[] = 4 5 1 2 3
+rotated3[] = 3 4 5 1 2
+sum[] = 12 10 8 6 9
+
+
+
+5. Write a program that finds the longest sequence of equal elements in an array of integers.
+If several longest sequences exist, print the leftmost one.
+Input Output
+2 1 1 2 3 3 2 2 2 1 2 2 2
+1 1 1 2 3 1 3 3 1 1 1
+4 4 4 4 4 4 4 4
+0 1 1 5 2 2 6 3 3 1 1
+
+
+using System;
+namespace day5csharp
+{
+    class day5csharp
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 3, 2, 1, 2, 3, 4, 3 };  
+            int startNum = 0, endNum = 0, maxCount = 0, count = 0, temp = 0;
+            int arryLen = arr.Length - 1;
+
+            for (int i = 0; i < arryLen; i++)
+            {
+                if (arr[i] == arr[i + 1] - 1)
+                {
+                    count++;
+                    maxCount = count;
+                    endNum = i + 1;
+                }
+                else
+                {
+                    temp = startNum;
+                    startNum = i + 1;
+                    count = 0;
+                }
+            }
+            for (int i = 0; i <= maxCount; i++)
+            {
+                Console.WriteLine(arr[temp + i]);
+            }
+        }
+    
+    }
+}
+
+
+
+7. Write a program that finds the most frequent number in a given sequence of numbers. In
+case of multiple numbers with the same maximal frequency, print the leftmost of them
+Input Output
+4 1 1 4 2 3 4 4 1 2 4 9 3 The number 4 is the most frequent (occurs 5 times)
+7 7 7 0 2 2 2 0 10 10 10 The numbers 2, 7 and 10 have the same maximal
+frequence (each occurs 3 times). The leftmost of them is 7.
+
+using System;
+namespace day5csharp
+{
+    class day5csharp
+    {
+        static void printNumber(int[] arr, int n, int k)
+        {
+            Dictionary<int, int> mp = new Dictionary<int, int>();
+  
+            for (int i = 0; i < n; i++)
+            {           
+                if (mp.ContainsKey(arr[i]))
+                {
+                    mp[arr[i]] = mp[arr[i]] + 1;
+                }
+                else
+                {
+                    mp.Add(arr[i], 1);
+                }
+            }
+
+            List<List<int>> freq = new List<List<int>>();
+
+            for (int i = 0; i <= n; i++)
+                freq.Add(new List<int>());
+            foreach (KeyValuePair<int, int> x in mp)
+                freq[x.Value].Add(x.Key);
+            int count = 0;
+            for (int i = n; i >= 0; i--)
+            {
+                foreach (int x in freq[i])
+                {
+                    Console.WriteLine("The number " + x + " is the most frequent");
+                    Console.WriteLine("It occurs " + i + " times");
+                    count++;
+                    if (count == k)
+                        return;
+                }
+            }
+        }
+        public static void Main(String[] args)
+        {
+            int[] arr = {2, 1, 1, 2, 3, 3, 2, 2, 2, 1};
+            int n = arr.Length;
+            int k = 1;
+            printNumber(arr, n, k);
+        }      
+    }
+}
+
+
+
+
+
+
+Practice Strings
+1. Write a program that reads a string from the console, reverses its letters and prints the
+result back at the console.
+Write in two ways
+Convert the string to char array, reverse it, then convert it to string again
+Print the letters of the string in back direction (from the last to the first) in a for-loop
+Input Output
+sample elpmas
+24tvcoi92 29iocvt42
+
+
+using System;
+namespace day5csharp
+{
+    class day5csharp
+    {
+        static void Main(string[] args)
+        {
+            string str, rser = "";
+            int i, j;
+            Console.Write("Input a string : ");
+            str = Console.ReadLine();
+            j = str.Length - 1;
+            for (i = j; i >= 0; i--)
+            {
+                rser += str[i];
+            }
+            Console.WriteLine("The reverse order is : {0}", rser);
+        }
+    }
+}
+
+
+2. Write a program that reverses the words in a given sentence without changing the
+punctuation and spaces
+Use the following separators between the words: . , : ; = ( ) & [ ] " ' \ / ! ? (space).
+All other characters are considered part of words, e.g. C++, a+b, and a77 are
+considered valid words.
+The sentences always start by word and end by separator.
+C# is not C++, and PHP is not Delphi!
+Delphi not is PHP, and C++ not is C#!
+The quick brown fox jumps over the lazy dog /Yes! Really!!!/.
+Really Yes dog lazy the over jumps fox brown /quick! The!!!/.
+
+using System;
+namespace day5csharp
+{
+    class day5csharp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Your words: ");
+            string words = Console.ReadLine();
+            string[] separ = words.Split(' ', '!', '.') ;
+            Stack<string> res = new Stack<string>(separ);
+            Console.Write("\nThe reserved words: ");
+
+            for (int i = 0; i < separ.Length; i++)
+            {
+                Console.Write($"{res.Pop()} ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
+
+3. Write a program that extracts from a given text all palindromes, e.g. “ABBA”, “lamal”, “exe”
+and prints them on the console on a single line, separated by comma and space.Print all
+unique palindromes (no duplicates), sorted
+Hi,exe? ABBA! Hog fully a string: ExE. Bob
+a, ABBA, exe, ExE
+
+
+using System;
+namespace day5csharp
+{
+    class day5csharp
+    {
+        static void Main(string[] args)
+        {
+            string str, pal = string.Empty;
+            Console.Write("Enter a word : ");
+            str = Console.ReadLine();
+
+            if (str != null)
+            {
+                int len = str.Length - 1;
+                for (int i = len; i >= 0; i--)
+                {
+                    pal += str[i].ToString();
+                }
+                if (pal == str)
+                {
+                    Console.WriteLine("The word " + pal + " is the palindrome ");
+                }
+                else
+                {
+                    Console.WriteLine("The word " + str + " is not the palindrome ");
+                }
+            }
+            Console.ReadLine();
+        }
+    
+    }
+}
+
+
+4. Write a program that parses an URL given in the following format:
+[protocol]://[server]/[resource]
+The parsing extracts its parts: protocol, server and resource.
+The [server] part is mandatory.
+The [protocol] and [resource] parts are optional.
+https://www.apple.com/iphone
+[protocol] = "https"
+[server] = "www.apple.com"
+[resource] = "iphone"
+ftp://www.example.com/employee
+[protocol] = "ftp"
+[server] = "www.example.com"
+[resource] = "employee"
+https://google.com
+[protocol] = "https"
+[server] = "google.com"
+[resource] = ""
+www.apple.com
+[protocol] = ""
+[server] = "www.apple.com"
+[resource] = ""
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace day5csharp
+{
+    class day5csharp
+    {
+        static void Main(string[] args)
+        {
+            String str = "ftp://www.example.com/employee";  //https://www.apple.com/iphone   https://google.com
+            String[] arr = new String[3];                    //ftp://www.example.com/employee
+            arr[0] = str.Substring(0, str.IndexOf("://"));
+            if (str.LastIndexOf('/') == str.IndexOf('/') )
+            {
+                arr[1] = str.Substring(str.IndexOf("www") );
+                arr[2] = "";
+            }
+            else
+            {
+                arr[1] = str.Substring(str.IndexOf("www"), str.LastIndexOf('/'));
+                arr[2] = str.Substring(str.LastIndexOf('/') );
+            }
+            Console.WriteLine("[protocol] = " + arr[0]);
+            Console.WriteLine("[server] = " + arr[1]);
+            Console.WriteLine("[resource] = " + arr[2]);
+        }
+
+    }
+}
+
 */
